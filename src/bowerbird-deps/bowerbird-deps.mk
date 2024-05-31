6b87422513f72a7f84ad7aaa1a5234cb7e5c85bd
Jeff Fredenburg <jfreden@fnal.gov>
2024-05-31 10:28:51 -0500

    removed the logic from the define-dependency-constant arguments

--- src/bowerbird-deps/bowerbird-deps.mk
+++ src/bowerbird-deps/bowerbird-deps.mk
@@ -1,18 +1,18 @@
-define bowerbird::git-dependency
-    $(eval $(call bowerbird::deps::define-dependency-constants,DEPENDENCY_$(basename $(notdir $(1))),$(1),$(2),$(3)))
-    $(DEPENDENCY_$(basename $(notdir $(1)))_DIR)/.:
+define bowerbird::git-dependency # url, vesion, entry
+    $(eval $(call bowerbird::deps::define-dependency-constants,DEPENDENCY_$(basename $(notdir $(1))),$(1),$(2),$(basename $(notdir $(1))),$(3)))
+    $(DEPENDENCY_$(basename $(notdir $(1)))_PATH)/.:
 		@echo "Cloning from '$($(1)_URL)'..."
 		@git clone --config advice.detachedHead=false --depth 1 \
 				--branch $(DEPENDENCY_$(basename $(notdir $(1)))_VERSION) \
 				$(DEPENDENCY_$(basename $(notdir $(1)))_URL) \
-				$(DEPENDENCY_$(basename $(notdir $(1)))_DIR)
+				$(DEPENDENCY_$(basename $(notdir $(1)))_PATH)
 
-    $(DEPENDENCY_$(basename $(notdir $(1))).MK): | $(DEPENDENCY_$(basename $(notdir $(1)))_DIR)/.
+    $(DEPENDENCY_$(basename $(notdir $(1))).MK): | $(DEPENDENCY_$(basename $(notdir $(1)))_PATH)/.
 		@test -d $$|
 		@test -f $$@ || (\
-			\rm -rf $(DEPENDENCY_$(basename $(notdir $(1)))_DIR) && \
+			\rm -rf $(DEPENDENCY_$(basename $(notdir $(1)))_PATH) && \
 			>&2 echo "\nERROR: Expected entry point not found: $$@ \
-			\nrm -rf $(DEPENDENCY_$(basename $(notdir $(1)))_DIR)\n" \
+			\nrm -rf $(DEPENDENCY_$(basename $(notdir $(1)))_PATH)\n" \
 			&& exit 1\
 		)
 		@echo
@@ -21,7 +21,7 @@ define bowerbird::git-dependency
 endef
 
 
-define bowerbird::deps::define-constant
+define bowerbird::deps::define-constant # name, value
     ifndef $(1)
         $(1) = $(2)
     else
@@ -31,10 +31,10 @@ define bowerbird::deps::define-constant
     endif
 endef
 
-define bowerbird::deps::define-dependency-constants
+define bowerbird::deps::define-dependency-constants # id, url, vesion, path, entry
     WORKDIR_DEPS ?= $$(error ERROR: Undefined variable WORKDIR_DEPS)
     $(eval $(call bowerbird::deps::define-constant,$(1)_URL,$(2)))
     $(eval $(call bowerbird::deps::define-constant,$(1)_VERSION,$(3)))
-    $(eval $(call bowerbird::deps::define-constant,$(1)_DIR,$(abspath $(WORKDIR_DEPS)/$(1))))
-    $(eval $(call bowerbird::deps::define-constant,$(1).MK,$(abspath $($(1)_DIR)/$(4))))
+    $(eval $(call bowerbird::deps::define-constant,$(1)_PATH,$(abspath $(WORKDIR_DEPS)/$(4))))
+    $(eval $(call bowerbird::deps::define-constant,$(1).MK,$(abspath $($(1)_PATH)/$(5))))
 endef
--- test/bowerbird-deps/test-define-dependency-constants.mk
+++ test/bowerbird-deps/test-define-dependency-constants.mk
@@ -1,26 +1,14 @@
 # Targets
 PHONY: test-define-dependency-constants
 test-define-dependency-constants: \
-		test-define-one-dependency-directory \
 		test-define-one-dependency-entry \
+		test-define-one-dependency-path \
 		test-define-one-dependency-url \
 		test-define-one-dependency-version \
-		test-define-second-dependency-conflicting-directory \
 		test-define-second-dependency-conflicting-entry \
+		test-define-second-dependency-conflicting-path \
 		test-define-second-dependency-conflicting-url \
 		test-define-second-dependency-conflicting-version \
-
-
-.PHONY: test-define-one-dependency-directory
-test-define-one-dependency-directory:
-	@$(MAKE) def-one-dependency-directory
-	@printf "\e[1;32mPassed: $(lastword $(MAKEFILE_LIST))::$@\e[0m\n"
-
-.PHONY:def-one-dependency-directory
-def-one-dependency-directory: WORKDIR_DEPS = deps/
-def-one-dependency-directory:
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/dependency.git,version,entry.mk))
-	@test $(ID_DIR) = $(abspath deps/ID)
 
 
 .PHONY: test-define-one-dependency-entry
@@ -31,8 +19,20 @@ test-define-one-dependency-entry:
 .PHONY:def-one-dependency-entry
 def-one-dependency-entry: WORKDIR_DEPS = deps/
 def-one-dependency-entry:
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/dependency.git,version,entry.mk))
-	@test $(ID.MK) = $(abspath deps/ID/entry.mk)
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,path,entry))
+	@test $(ID.MK) = $(abspath deps/path/entry)
+
+
+.PHONY: test-define-one-dependency-path
+test-define-one-dependency-path:
+	@$(MAKE) def-one-dependency-path
+	@printf "\e[1;32mPassed: $(lastword $(MAKEFILE_LIST))::$@\e[0m\n"
+
+.PHONY:def-one-dependency-path
+def-one-dependency-path: WORKDIR_DEPS = deps/
+def-one-dependency-path:
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,path,entry))
+	@test $(ID_PATH) = $(abspath deps/path)
 
 
 .PHONY: test-define-one-dependency-url
@@ -43,8 +43,8 @@ test-define-one-dependency-url:
 .PHONY:def-one-dependency-url
 def-one-dependency-url: WORKDIR_DEPS = deps/
 def-one-dependency-url:
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/dependency.git,version,entry.mk))
-	@test $(ID_URL) = url/dependency.git
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,path,entry))
+	@test $(ID_URL) = url
 
 
 .PHONY: test-define-one-dependency-version
@@ -55,20 +55,8 @@ test-define-one-dependency-version:
 .PHONY:def-one-dependency-version
 def-one-dependency-version: WORKDIR_DEPS = deps/
 def-one-dependency-version:
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/dependency.git,version,entry.mk))
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,path,entry))
 	@test $(ID_VERSION) = version
-
-
-.PHONY: test-define-second-dependency-conflicting-directory
-test-define-second-dependency-conflicting-directory:
-	@! $(MAKE) def-second-dependency-conflicting-directory 2>/dev/null
-	@printf "\e[1;32mPassed: $(lastword $(MAKEFILE_LIST))::$@\e[0m\n"
-
-.PHONY:def-second-dependency-conflicting-directory
-def-second-dependency-conflicting-directory: WORKDIR_DEPS = deps/
-def-second-dependency-conflicting-directory:
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/alpha.git,version,entry.mk))
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/beta.git,version,entry.mk))
 
 
 .PHONY: test-define-second-dependency-conflicting-entry
@@ -79,8 +67,20 @@ test-define-second-dependency-conflicting-entry:
 .PHONY:def-second-dependency-conflicting-entry
 def-second-dependency-conflicting-entry: WORKDIR_DEPS = deps/
 def-second-dependency-conflicting-entry:
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/dependency.git,version,alpha.mk))
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/dependency.git,version,beta.mk))
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,entry/alpha))
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,entry/beta))
+
+
+.PHONY: test-define-second-dependency-conflicting-path
+test-define-second-dependency-conflicting-path:
+	@! $(MAKE) def-second-dependency-conflicting-path 2>/dev/null
+	@printf "\e[1;32mPassed: $(lastword $(MAKEFILE_LIST))::$@\e[0m\n"
+
+.PHONY:def-second-dependency-conflicting-path
+def-second-dependency-conflicting-path: WORKDIR_DEPS = deps/
+def-second-dependency-conflicting-path:
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,path/alpha,version,entry))
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,path/beta,version,entry))
 
 
 .PHONY: test-define-second-dependency-conflicting-url
@@ -91,9 +91,8 @@ test-define-second-dependency-conflicting-url:
 .PHONY:def-second-dependency-conflicting-url
 def-second-dependency-conflicting-url: WORKDIR_DEPS = deps/
 def-second-dependency-conflicting-url:
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/alpha.git,version,entry.mk))
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/beta.git,version,entry.mk))
-	@test $(ID_URL) = url/dependency.git
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/alpha,version,entry))
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/beta,version,entry))
 
 
 .PHONY: test-define-second-dependency-conflicting-version
@@ -104,5 +103,5 @@ test-define-second-dependency-conflicting-version:
 .PHONY:def-second-dependency-conflicting-version
 def-second-dependency-conflicting-version: WORKDIR_DEPS = deps/
 def-second-dependency-conflicting-version:
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/dependency.git,alpha,entry.mk))
-	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url/dependency.git,beta,entry.mk))
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,path,version/alpha,entry))
+	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,path,version/beta,entry))
