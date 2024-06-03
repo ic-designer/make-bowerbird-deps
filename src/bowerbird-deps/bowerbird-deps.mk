ca080a230424b76be4edbf0e559b2da9c46cfe3e
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-03 08:21:02 -0500

    cleaned up the variables are created in the macros

--- src/bowerbird-deps/bowerbird-deps.mk
+++ src/bowerbird-deps/bowerbird-deps.mk
@@ -1,42 +1,42 @@
 define bowerbird::git-dependency # id, url, vesion, entry
-    $(eval $(call bowerbird::deps::define-dependency-constants,_DEP_$(1),$(2),$(3),$(1),$(4)))
-    $(_DEP_$(1)_PATH)/.:
-		@echo "Cloning from '$(_DEP_$(1)_URL)'..." >&2
+    $(eval $(call bowerbird::deps::define-dependency-constants,BOWERBIRD_DEPENDENCY/$1,$2,$3,$1,$4))
+    $(BOWERBIRD_DEPENDENCY/$1/PATH)/.:
+		@echo "Cloning from '$(BOWERBIRD_DEPENDENCY/$1/URL)'..." >&2
 		@git clone --config advice.detachedHead=false --depth 1 \
-				--branch $(_DEP_$(1)_VERSION) \
-				$(_DEP_$(1)_URL) \
-				$(_DEP_$(1)_PATH)
-		@rm -rf $(_DEP_$(1)_PATH)/.git
+				--branch $(BOWERBIRD_DEPENDENCY/$1/VERSION) \
+				$(BOWERBIRD_DEPENDENCY/$1/URL) \
+				$(BOWERBIRD_DEPENDENCY/$1/PATH)
+		@rm -rf $(BOWERBIRD_DEPENDENCY/$1/PATH)/.git
 
 
-    $(_DEP_$(1).MK): | $(_DEP_$(1)_PATH)/.
+    $(BOWERBIRD_DEPENDENCY/$1.MK): | $(BOWERBIRD_DEPENDENCY/$1/PATH)/.
 		@test -d $$|
 		@test -f $$@ || (\
-			\rm -rf $(_DEP_$(1)_PATH) && \
+			\rm -rf $(BOWERBIRD_DEPENDENCY/$1/PATH) && \
 			>&2 echo "\nERROR: Expected entry point not found: $$@ \
-			\nrm -rf $(_DEP_$(1)_PATH)\n" \
+			\nrm -rf $(BOWERBIRD_DEPENDENCY/$1/PATH)\n" \
 			&& exit 1\
 		)
 		@echo
 
-    include $(_DEP_$(1).MK)
+    include $(BOWERBIRD_DEPENDENCY/$1.MK)
 endef
 
 
 define bowerbird::deps::define-constant # name, value
-    ifndef $(1)
-        $(1) = $(2)
+    ifndef $1
+        $1 = $2
     else
-        ifneq ($($(1)),$(2))
-            $$(error ERROR: Identified conflict with $(1): '$($(1))' != '$(2)' )
+        ifneq ($($1),$2)
+            $$(error ERROR: Identified conflict with $1: '$($1)' != '$2' )
         endif
     endif
 endef
 
 define bowerbird::deps::define-dependency-constants # id, url, vesion, path, entry
     WORKDIR_DEPS ?= $$(error ERROR: Undefined variable WORKDIR_DEPS)
-    $(eval $(call bowerbird::deps::define-constant,$(1)_URL,$(2)))
-    $(eval $(call bowerbird::deps::define-constant,$(1)_VERSION,$(3)))
-    $(eval $(call bowerbird::deps::define-constant,$(1)_PATH,$(abspath $(WORKDIR_DEPS)/$(4))))
-    $(eval $(call bowerbird::deps::define-constant,$(1).MK,$(abspath $($(1)_PATH)/$(5))))
+    $(eval $(call bowerbird::deps::define-constant,$1/URL,$2))
+    $(eval $(call bowerbird::deps::define-constant,$1/VERSION,$3))
+    $(eval $(call bowerbird::deps::define-constant,$1/PATH,$(abspath $(WORKDIR_DEPS)/$4)))
+    $(eval $(call bowerbird::deps::define-constant,$1.MK,$(abspath $($1/PATH)/$5)))
 endef
