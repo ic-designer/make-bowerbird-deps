b8c3c0c25a2401850e0be789cfa5dca87b44c712
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-04 21:48:54 -0500

    fixed some issues in the git-dependency macro which causes undefined variables and also changed ID to the installation path

--- src/bowerbird-deps/bowerbird-deps.mk
+++ src/bowerbird-deps/bowerbird-deps.mk
@@ -1,34 +1,34 @@
-define bowerbird::git-dependency # id, url, vesion, entry
-    $(eval $(call bowerbird::deps::define-dependency-constants,BOWERBIRD_DEPENDENCY/$1,$2,$3,$1,$4))
-    $(BOWERBIRD_DEPENDENCY/$1/PATH)/.:
+define bowerbird::git-dependency # path, url, vesion, entry
+    $$(eval $$(call bowerbird::deps::define-dependency-constants,BOWERBIRD_DEPENDENCY/$1,$2,$3,$1,$4))
+    $$(BOWERBIRD_DEPENDENCY/$1/PATH)/.:
 		@git clone --config advice.detachedHead=false --depth 1 \
-				--branch $(BOWERBIRD_DEPENDENCY/$1/VERSION) \
-				$(BOWERBIRD_DEPENDENCY/$1/URL) \
-				$(BOWERBIRD_DEPENDENCY/$1/PATH) \
+				--branch $$(BOWERBIRD_DEPENDENCY/$1/VERSION) \
+				$$(BOWERBIRD_DEPENDENCY/$1/URL) \
+				$$(BOWERBIRD_DEPENDENCY/$1/PATH) \
                 1>/dev/null \
                 2>/dev/null || \
-        (echo ERROR: Failed to clone dependency '$(BOWERBIRD_DEPENDENCY/$1/URL)' && exit 1)
-		@test -n $(BOWERBIRD_DEPENDENCY/$1/PATH)
-		@test -d $(BOWERBIRD_DEPENDENCY/$1/PATH)/.git
-		@rm -rf $(BOWERBIRD_DEPENDENCY/$1/PATH)/.git
+        (echo ERROR: Failed to clone dependency '$$(BOWERBIRD_DEPENDENCY/$1/URL)' && exit 1)
+		@test -n $$(BOWERBIRD_DEPENDENCY/$1/PATH)
+		@test -d $$(BOWERBIRD_DEPENDENCY/$1/PATH)/.git
+		@rm -rf $$(BOWERBIRD_DEPENDENCY/$1/PATH)/.git
 
 
-    $(BOWERBIRD_DEPENDENCY/$1.MK): | $(BOWERBIRD_DEPENDENCY/$1/PATH)/.
+    $$(BOWERBIRD_DEPENDENCY/$1.MK): | $$(BOWERBIRD_DEPENDENCY/$1/PATH)/.
 		@test -d $$|
 		@test -f $$@ || (\
-			\rm -rf $(BOWERBIRD_DEPENDENCY/$1/PATH) && \
+			\rm -rf $$(BOWERBIRD_DEPENDENCY/$1/PATH) && \
 			>&2 echo "\nERROR: Expected entry point not found: $$@ \
-			\nrm -rf $(BOWERBIRD_DEPENDENCY/$1/PATH)\n" \
+			\nrm -rf $$(BOWERBIRD_DEPENDENCY/$1/PATH)\n" \
 			&& exit 1\
 		)
 
-    include $(BOWERBIRD_DEPENDENCY/$1.MK)
+    include $$(BOWERBIRD_DEPENDENCY/$1.MK)
 endef
 
 
 define bowerbird::deps::define-constant # name, value
     ifndef $1
-        $1 = $2
+        $1 := $2
     else
         ifneq ($$($1),$2)
             $$(error ERROR: Identified conflict with $1: '$$($1)' != '$2' )
@@ -37,9 +37,8 @@ define bowerbird::deps::define-constant # name, value
 endef
 
 define bowerbird::deps::define-dependency-constants # id, url, vesion, path, entry
-    WORKDIR_DEPS ?= $$(error ERROR: Undefined variable WORKDIR_DEPS)
-    $(eval $(call bowerbird::deps::define-constant,$1/URL,$2))
-    $(eval $(call bowerbird::deps::define-constant,$1/VERSION,$3))
-    $(eval $(call bowerbird::deps::define-constant,$1/PATH,$(abspath $(WORKDIR_DEPS)/$4)))
-    $(eval $(call bowerbird::deps::define-constant,$1.MK,$(abspath $($1/PATH)/$5)))
+    $$(eval $$(call bowerbird::deps::define-constant,$1/URL,$2))
+    $$(eval $$(call bowerbird::deps::define-constant,$1/VERSION,$3))
+    $$(eval $$(call bowerbird::deps::define-constant,$1/PATH,$$(abspath $4)))
+    $$(eval $$(call bowerbird::deps::define-constant,$1.MK,$$($1/PATH)/$5))
 endef
