c2277bf60aeb4aa6e276d18de8fc71266154a907
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-04 21:50:12 -0500

    updated the dependency calls in make/deps.mk to use the explicit install path

--- make/deps.mk
+++ make/deps.mk
@@ -3,6 +3,6 @@ WORKDIR_DEPS ?= $(error ERROR: Undefined variable WORKDIR_DEPS)
 
 # Load Dependencies
 include src/bowerbird-deps/bowerbird-deps.mk
-$(eval $(call bowerbird::git-dependency,BOWERBIRD_HELP,https://github.com/ic-designer/make-bowerbird-help.git,main,bowerbird.mk))
-$(eval $(call bowerbird::git-dependency,BOWERBIRD_GIT,https://github.com/ic-designer/make-bowerbird-git.git,main,bowerbird.mk))
-$(eval $(call bowerbird::git-dependency,BOWERBIRD_TEST,https://github.com/ic-designer/make-bowerbird-test.git,main,bowerbird.mk))
+$(eval $(call bowerbird::git-dependency,$(WORKDIR_DEPS)/bowerbird-help,https://github.com/ic-designer/make-bowerbird-help.git,main,bowerbird.mk))
+$(eval $(call bowerbird::git-dependency,$(WORKDIR_DEPS)/bowerbird-githooks,https://github.com/ic-designer/make-bowerbird-githooks.git,main,bowerbird.mk))
+$(eval $(call bowerbird::git-dependency,$(WORKDIR_DEPS)/bowerbird-test,https://github.com/ic-designer/make-bowerbird-test.git,main,bowerbird.mk))
