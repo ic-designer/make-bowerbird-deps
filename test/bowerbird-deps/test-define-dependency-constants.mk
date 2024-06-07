3c721ee27e4b13ef1712fb68e529e351f6718fca
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-06 22:45:46 -0500

    updated calls using 'compare-string' to the updates 'compare-strings'

--- test/bowerbird-deps/test-define-dependency-constants.mk
+++ test/bowerbird-deps/test-define-dependency-constants.mk
@@ -4,7 +4,7 @@ test-define-one-dependency-entry:
 .PHONY: def-one-dependency-entry
 def-one-dependency-entry:
 	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,path,entry))
-	$(call bowerbird::test::compare-string,$(ID.MK),$(abspath path/entry))
+	$(call bowerbird::test::compare-strings,$(ID.MK),$(abspath path/entry))
 
 
 test-define-one-dependency-path:
@@ -13,7 +13,7 @@ test-define-one-dependency-path:
 .PHONY: def-one-dependency-path
 def-one-dependency-path:
 	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,path,entry))
-	$(call bowerbird::test::compare-string,$(ID/PATH),$(abspath path))
+	$(call bowerbird::test::compare-strings,$(ID/PATH),$(abspath path))
 
 
 test-define-one-dependency-url:
@@ -22,7 +22,7 @@ test-define-one-dependency-url:
 .PHONY: def-one-dependency-url
 def-one-dependency-url:
 	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,path,entry))
-	$(call bowerbird::test::compare-string,$(ID/URL),url)
+	$(call bowerbird::test::compare-strings,$(ID/URL),url)
 
 
 test-define-one-dependency-version:
@@ -31,7 +31,7 @@ test-define-one-dependency-version:
 .PHONY: def-one-dependency-version
 def-one-dependency-version:
 	$(eval $(call bowerbird::deps::define-dependency-constants,ID,url,version,path,entry))
-	$(call bowerbird::test::compare-string,$(ID/VERSION),version)
+	$(call bowerbird::test::compare-strings,$(ID/VERSION),version)
 
 
 test-define-second-dependency-conflicting-entry:
