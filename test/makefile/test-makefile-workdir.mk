1b49ee88cf38ba54c6da637b31e20b48207f3cff
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-17 21:01:23 -0500

    revert workdir tests back to using target-specific variables

--- test/makefile/test-makefile-workdir.mk
+++ test/makefile/test-makefile-workdir.mk
@@ -1,13 +1,19 @@
-
 test-workdir-root:
-	$(MAKE) bowerbird/deps/test-makefile-workdir/force \
-		WORKDIR_ROOT=$(WORKDIR_TEST)/$@/root
-	test -d $(WORKDIR_TEST)/$@/root/deps
+	$(call bashargs::test-workdir::build-helper)
 
+test-workdir-deps: WORKDIR_DEPS=deps
 test-workdir-deps:
-	$(MAKE) bowerbird/deps/test-makefile-workdir/force \
-		WORKDIR_DEPS=$(WORKDIR_TEST)/$@/deps
-	test -d $(WORKDIR_TEST)/$@/deps
+	test "$(WORKDIR_DEPS)" = "deps"
+	$(call bashargs::test-workdir::build-helper)
 
-bowerbird/deps/test-makefile-workdir/force:
+
+make-bowerbird/test-makefile-workdir/force:
 	@:
+
+define bashargs::test-workdir::build-helper
+	$(MAKE) make-bowerbird/test-makefile-workdir/force \
+			WORKDIR_ROOT=$(WORKDIR_TEST)/$@ \
+			WORKDIR_DEPS=$(WORKDIR_TEST)/$@/$(WORKDIR_DEPS)
+	test -d $(WORKDIR_TEST)/$@/$(WORKDIR_DEPS)
+	test -d $(WORKDIR_TEST)/$@/$(WORKDIR_DEPS)
+endef
